02d3958ad0466c0c2e8f0380f89bc87f43ad77b4
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-03-24 23:46:16 +0400

    Add start workers script

---
+++ deployment-resources/start-workers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ssh ego225@10.230.12.41 -p 4410 'bash -ic "./capstone-project/worker 10.230.12.40:9092 worker1 &> /dev/null"' &
+ssh ego225@10.230.12.42 -p 4410 'bash -ic "./capstone-project/worker 10.230.12.40:9092 worker2 &> /dev/null"' &
+ssh ego225@10.230.12.43 -p 4410 'bash -ic "./capstone-project/worker 10.230.12.40:9092 worker3 &> /dev/null"' &
+ssh ego225@10.230.12.44 -p 4410 'bash -ic "./capstone-project/worker 10.230.12.40:9092 worker4 &> /dev/null"' &
